98c56eec40038223ceae166d98c21403ce5d9fb7
System Administrator <ahh2131@columbia.edu>
2014-02-25 21:50:56 -0500

    Build file

---
+++ build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_app_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleAppInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { APP_INFO_CURRENT_STRUCT_VERSION_MAJOR, APP_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { APP_INFO_CURRENT_SDK_VERSION_MAJOR, APP_INFO_CURRENT_SDK_VERSION_MINOR },
+  .app_version = { 2, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "pebmo",
+  .company = "Andy Hadjigeorgiou",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = 0,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x69, 0xA5, 0x39, 0x87, 0x37, 0x9E, 0x4A, 0xE7, 0xA3, 0xC2, 0xD0, 0x8E, 0x5F, 0xFE, 0x85, 0x11 },
+  .virtual_size = 0xb6b6
+};
